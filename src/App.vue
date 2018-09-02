58aba49b814841a4e61b49ed3ce1b45ef5087a91
Aleksey Belchenkov <u608110@gmail.com>
2018-09-02 09:08:08 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>Hello</h1>
+    <button class="btn btn-dark">Hello</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
