2db47ae68b34f11ba0dd7e6b73dbc66576c61e30
Aleksey Belchenkov <u608110@gmail.com>
2018-09-02 10:30:29 +0300

    Creating the app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,57 @@
 <template>
-  <div class="container">
-    <h1>Hello</h1>
-    <button class="btn btn-dark">Hello</button>
+  <div class="container" id="app">
+    <div class="row">
+        <h1 class="text-center">The Super Quiz</h1>
+        <hr>
+    </div>
+    <hr>
+    <div class="row">
+      <div>
+        <component
+          :is="mode"
+          @answered="answered($event)"
+          @confirmed="mode = 'app-question'"
+        ></component>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Question from './components/Question';
+  import Answer from './components/Answer';
+
 export default {
-  name: 'App'
+  data () {
+    return {
+      mode: 'app-question'
+    }
+  },
+  methods: {
+    answered (isCorrect) {
+      if (isCorrect) {
+        this.mode = 'app-answer';
+      } else {
+        this.mode = 'app-question';
+        alert('Wrong, try again!');
+      }
+    }
+  },
+  components: {
+    appQuestion: Question,
+    appAnswer: Answer
+  }
 }
 </script>
 
 <style>
-
+  #app {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    padding-bottom: 250px;
+  }
 </style>
